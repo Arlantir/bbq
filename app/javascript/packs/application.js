7039f5b99cce172e94db0f76105c04c611939889
Arlantir <ror@worldoflosttime.ru>
2020-02-20 01:21:46 +0300

    setting bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap/dist/js/bootstrap")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,4 +16,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'bootstrap'
+import './bootstrap_custom.js'
