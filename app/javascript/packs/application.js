6120d5f93958ce671bf20a33f099fe137e549e60
Arlantir <ror@worldoflosttime.ru>
2020-02-21 23:19:09 +0300

    fix bootstrap and add users views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("bootstrap/dist/js/bootstrap")
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap/dist/js/bootstrap")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,5 +14,4 @@ require("bootstrap/dist/js/bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import './bootstrap_custom.js'
+import '../stylesheets/application'
