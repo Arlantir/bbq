23dbb7d6a80b795d987197f0e4017383f2755be9
Arlantir <ror@worldoflosttime.ru>
2020-02-26 00:07:14 +0300

    fix remarks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,18 +10,15 @@ class UsersController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: I18n.t('controllers.users.updated') }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: I18n.t('controllers.users.updated')
+    else
+      render :edit
     end
   end
 
   private
+
   def set_current_user
     @user = current_user
   end
