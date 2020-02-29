c1f28f88874ceac1acc218fa36b857e3a794223f
Arlantir <ror@worldoflosttime.ru>
2020-03-01 02:09:00 +0300

    fix belongs_to subscription user

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_222544) do
     t.string "user_name"
     t.string "user_email"
     t.bigint "event_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_subscriptions_on_event_id"
