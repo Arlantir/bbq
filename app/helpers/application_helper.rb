8be91e742b9f2125653ae8f5a5e151adec3b3fda
Arlantir <ror@worldoflosttime.ru>
2020-03-06 20:29:15 +0300

    setting add avatar in profile user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,11 @@ module ApplicationHelper
   end
 
   def user_avatar(user)
-    asset_path('user.png')
+    if user.avatar?
+      user.avatar.url
+    else
+      asset_path('user.png')
+    end
   end
 
   def fa_icon(icon_class)
