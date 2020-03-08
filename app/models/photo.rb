7446d6ae274a6848976a7223927379772e9154c0
Arlantir <ror@worldoflosttime.ru>
2020-03-09 02:11:48 +0300

    big fix display photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,8 @@ class Photo < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
+  validates :photo, presence: true
+
   mount_uploader :photo, PhotoUploader
 
   # Scope нужен, чтобы отделить реальные фотки от болванки,
