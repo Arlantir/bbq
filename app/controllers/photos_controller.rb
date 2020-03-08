4fd4e7be30be810d99559a1c90008aa3a24a1eb0
Arlantir <ror@worldoflosttime.ru>
2020-03-08 03:07:56 +0300

    setting controller photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,74 +1,58 @@
 class PhotosController < ApplicationController
-  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: %i[create destroy]
+  before_action :set_photo, only: %i[destroy]
 
-  # GET /photos
-  # GET /photos.json
-  def index
-    @photos = Photo.all
-  end
-
-  # GET /photos/1
-  # GET /photos/1.json
-  def show
-  end
-
-  # GET /photos/new
-  def new
-    @photo = Photo.new
-  end
-
-  # GET /photos/1/edit
-  def edit
-  end
-
-  # POST /photos
-  # POST /photos.json
+  # Обратите внимание: фотку может сейчас добавить даже неавторизованный пользовать
+  # Смотрите домашки!
   def create
-    @photo = Photo.new(photo_params)
+    # Создаем новую фотографию у нужного события @event
+    @new_photo = @event.photos.build(photo_params)
 
-    respond_to do |format|
-      if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
-        format.json { render :show, status: :created, location: @photo }
-      else
-        format.html { render :new }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
+    # Проставляем у фотографии пользователя
+    @new_photo.user = current_user
+
+    if @new_photo.save
+      # Если фотка сохранилась, редиректим на событие с сообщением
+      redirect_to @event, notice: I18n.t('controllers.photos.created')
+    else
+      # Если нет — рендерим событие с ошибкой
+      render 'events/show', alert: I18n.t('controllers.photos.error')
     end
   end
 
-  # PATCH/PUT /photos/1
-  # PATCH/PUT /photos/1.json
-  def update
-    respond_to do |format|
-      if @photo.update(photo_params)
-        format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @photo }
-      else
-        format.html { render :edit }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /photos/1
-  # DELETE /photos/1.json
   def destroy
-    @photo.destroy
-    respond_to do |format|
-      format.html { redirect_to photos_url, notice: 'Photo was successfully destroyed.' }
-      format.json { head :no_content }
+    message = { notice: I18n.t('controllers.photos.destroyed') }
+
+    # Проверяем, может ли пользователь удалить фотографию
+    # Если может — удаляем
+    if current_user_can_edit?(@photo)
+      @photo.destroy
+    else
+      # Если нет — сообщаем ему
+      message = { alert: I18n.t('controllers.photos.error') }
     end
+
+    # В любом случае редиректим юзера на событие
+    redirect_to @event, message
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_photo
-      @photo = Photo.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def photo_params
-      params.fetch(:photo, {})
-    end
+  # Так как фотография — вложенный ресурс, в params[:event_id] рельсы
+  # автоматически положат id события, которому принадлежит фотография
+  # Это событие будет лежать в переменной контроллера @event
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  # Получаем фотографию из базы стандартным методом find
+  def set_photo
+    @photo = @event.photos.find(params[:id])
+  end
+
+  # При создании новой фотографии мы получаем массив параметров photo
+  # c единственным полем photo
+  def photo_params
+    params.fetch(:photo, {}).permit(:photo)
+  end
 end
