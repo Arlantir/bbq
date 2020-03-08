a1935675df8f0190838f0f031818dff24c0e7e11
Arlantir <ror@worldoflosttime.ru>
2020-03-08 03:35:26 +0300

    setting view thumb photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,8 @@ class Photo < ApplicationRecord
   belongs_to :user
 
   mount_uploader :photo, PhotoUploader
+
+  # Scope нужен, чтобы отделить реальные фотки от болванки,
+  # которую мы прописали в контроллере событий
+  scope :persisted, -> { where "id IS NOT NULL" }
 end
