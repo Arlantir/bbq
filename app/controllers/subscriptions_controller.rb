695fbfd3fcfaee28a75740315a646f5a8e36b52c
Arlantir <ror@worldoflosttime.ru>
2020-03-08 15:35:49 +0300

    fix - add now for flash

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -12,7 +12,7 @@ class SubscriptionsController < ApplicationController
       redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
     else
       # сообщение об ошибке, если юзер пытается подписаться на своё событие
-      flash[:error] = @new_subscription.errors.full_messages
+      flash.now[:error] = @new_subscription.errors.full_messages
       # если ошибки — рендерим шаблон события
       render 'events/show', alert: I18n.t('controllers.subscriptions.error')
     end
