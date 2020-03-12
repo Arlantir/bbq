2b6a914dd9d11725ce079d9bc2e300ae31bd0abc
Arlantir <ror@worldoflosttime.ru>
2020-03-13 01:27:12 +0300

    add reset password

---
+++ app/mailers/event_mailer.rb
@@ -0,0 +1,14 @@
+class EventMailer < ApplicationMailer
+
+  def subscription
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  def comment
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
