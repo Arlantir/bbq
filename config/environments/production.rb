b238718b1b369d6502ae36ee6cd0d5c8527f39db
Arlantir <ror@worldoflosttime.ru>
2020-03-15 00:05:39 +0300

    add setting sengrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,17 +123,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   # А это для SendGrid
-  #  TODO они там неадекватные, банят сразу при добавлении аддона на хероку
-  # ActionMailer::Base.smtp_settings = {
-  #   :user_name => ENV['SENDGRID_USERNAME'],
-  #   :password => ENV['SENDGRID_PASSWORD'],
-  #   :domain => 'megobbq.herokuapp.com',
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => 587,
-  #   :authentication => :plain,
-  #   :enable_starttls_auto => true
-  # }
-
-  # А это для Mailtrap
-
+  # TODO они там неадекватные, банят сразу при добавлении аддона на хероку
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'megobbq.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
