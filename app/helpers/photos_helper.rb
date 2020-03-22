65308004e5d8e61e36051408374914416b2e4e47
Arlantir <ror@worldoflosttime.ru>
2020-03-23 00:22:28 +0300

    fix helper photo

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,7 +1,10 @@
 module PhotosHelper
   def image_title(photo)
-    image_title = t('photo.photo'), " #{render partial: 'users/user', object: photo.user}",
-      current_user_can_edit?(photo) ? link_to(fa_icon('trash'), event_photo_path(photo.event, photo),
-                                              method: :delete, data: {confirm: t('photo.confirm')}) : ''
+    <<-INFO_USER
+      #{t('photo.photo')}
+      #{render partial: 'users/user', object: photo.user}
+      #{current_user_can_edit?(photo) ? link_to(fa_icon('trash'), event_photo_path(photo.event, photo),
+                                                method: :delete, data: {confirm: t('photo.confirm')}) : ''}
+    INFO_USER
   end
 end
