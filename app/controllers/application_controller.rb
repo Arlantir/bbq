154844c08e16df97214d5b5361cc664c268623cc
Arlantir <ror@worldoflosttime.ru>
2020-04-12 17:27:35 +0300

    add name fo registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_can_edit?
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
     devise_parameter_sanitizer.permit(
       :account_update,
       keys: [:password, :password_confirmation, :current_password]
