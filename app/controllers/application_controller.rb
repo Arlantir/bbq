192834e154500834688cfbead7764d1e51dc8a6c
Arlantir <ror@worldoflosttime.ru>
2020-04-12 20:39:17 +0300

    fix cap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def current_user_can_edit?(model)
     # Если у модели есть юзер и он залогиненный, пробуем у неё взять .event
-    # Если он есть, проверяем его юзера на равенство current_user.
+    # Если он есть, проверяем  его юзера на равенство current_user.
     user_signed_in? && (
     model.user == current_user ||
       (model.try(:event).present? && model.event.user == current_user)
